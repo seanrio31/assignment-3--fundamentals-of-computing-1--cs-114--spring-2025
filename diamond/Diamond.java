c771e2430459a79950a3d7a2840b8bc1be20d48a
seanrio31 <sriordan@hartford.edu>
2025-04-10 10:38:45 -0400

    Add work

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the size of the diamond: ");
+    int size = scanner.nextInt();
+
+    if (size < 1) {
+      System.out.println("Please enter a number greater than 0");
+      return;
+    }
+
+    if (size % 2 == 0) {
+      size += 1;
+    }
+
+    int mid = size / 2;
+
+    for (int i = 0; i <= mid; i++) {
+      for (int j = 0; j < mid - i; j++) {
+        System.out.print(" ");
+      }
+
+      for (int j = 0; j < 2 * i + 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    for (int i = mid - 1; i >= 0; i--) {
+      for (int j = 0; j < mid - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < 2 * i + 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    scanner.close();
   }
 }
--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,53 @@
 public class Matrix {
+  private int [][] matrix;
+
+  public Matrix(int size) {
+    matrix = new int [size][size];
+    System.out.println("Matrix dimensions: " + size + "x" = size);
+  }
+
+  private void swap(int x1, int y1, int x2, int y2){
+    int temp = matrix[x1][y1];
+    matrix[x1][y2] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+  public void printMatrix() {
+    for (int i=0; i < matrix.length; i++) {
+      for (int j=0; j < matrix[i].length; j++){
+        if (i +j == matrix.length - 1){
+          System.out.print("\033[33m" + matrix[i][j] + "\033[0m" + " ");
+        } else {
+          System.out.print(matrix[i][j] + " ");
+        }
+      }
+      System.out.println(matrix[i][j] + " ");
+    }
+  }
+  public void populateMatrix(){
+    int value = 1;
+    for (int i = 0; i < matrix.length; i++) {
+      for int j = 0; j < matrix[i].length; j++ {
+        matrix[i][j] = value++;
+      }
+    }
+  }
+  public void flipMatrix() {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = matrix[i].length - 1; j >= 0; j--) {
+        if (i + j == matrix.length - 1) {
+          continue;
+        }
+        swap(i, j, matrix.length - 1 - j, matrix.length -1 - i);
+      }
+    }
+  }
+  public static void main(String[] args) {
+    Matrix matrix = new Matrix(5);
+    matrix.populateMatrix();
+    System.out.println("Original Matrix:");
+    matrix.printMatrix();
+    matrix.flipMatrix();
+    System.out.println("Flipped Matrix:");
+    matrix.printMatrix();
+  }
 }
