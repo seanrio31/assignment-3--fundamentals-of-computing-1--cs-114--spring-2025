54d46d03565ea100a5344738c19e330762610642
seanrio31 <sriordan@hartford.edu>
2025-04-10 10:49:56 -0400

    Move method main code from Matrix.java to RunMatrix.java

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -42,13 +42,4 @@ public class Matrix {
       }
     }
   }
-  public static void main(String[] args) {
-    Matrix matrix = new Matrix(5);
-    matrix.populateMatrix();
-    System.out.println("Original Matrix:");
-    matrix.printMatrix();
-    matrix.flipMatrix();
-    System.out.println("Flipped Matrix:");
-    matrix.printMatrix();
-  }
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Matrix matrix = new Matrix(5);
+    matrix.populateMatrix();
+    System.out.println("Original Matrix:");
+    matrix.printMatrix();
+    matrix.flipMatrix();
+    System.out.println("Flipped Matrix:");
+    matrix.printMatrix();
   }
 }
